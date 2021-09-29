003a264d3ff2fc79b522b92bf78f878d89f3091a
mdodovic <matija.dodovic@gmail.com>
2021-09-29 15:57:51 +0200

    initial commit

---
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class List : PageModel
+    {
+        
+        public string Message { get; set; }
+        
+        public void OnGet()
+        {
+            Message = "Hello, World!";
+        }
+    }
+}
