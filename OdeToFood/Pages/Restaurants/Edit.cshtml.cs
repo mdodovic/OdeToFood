700a83dd01e51003b6a8ac2cccf278d2d00fb0c9
mdodovic <matija.dodovic@gmail.com>
2021-09-30 13:20:09 +0200

    add Edit

---
+++ OdeToFood/Pages/Restaurants/Edit.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFood.Core;
+using OdeToFood.Data;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class Edit : PageModel
+    {
+
+        private readonly IRestaurantData restaurantData;
+        public Restaurant Restaurant { get; set; }
+        
+        public Edit(IRestaurantData restaurantData)
+        {
+            this.restaurantData = restaurantData;
+        }
+        
+        public IActionResult OnGet(int restaurantId)
+        {
+            Restaurant = restaurantData.GetById(restaurantId);
+            if (Restaurant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+            return Page();
+        }
+    }
+}
