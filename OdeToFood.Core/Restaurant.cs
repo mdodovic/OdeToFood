875372dc201b38c45a30ac7c195feee547fb2807
mdodovic <matija.dodovic@gmail.com>
2021-09-30 09:31:02 +0200

    added cuisine field

--- OdeToFood.Core/Restaurant.cs
+++ OdeToFood.Core/Restaurant.cs
@@ -5,8 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Location { get; set; }
-        
-        
+        public CuisineType Cuisine { get; set; }
         
     }
 }
