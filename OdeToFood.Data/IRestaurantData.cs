a7d67ad5ecb279b23f443824b366b325c1a11b2d
mdodovic <matija.dodovic@gmail.com>
2021-09-30 11:04:37 +0200

    add search by name

--- OdeToFood.Data/IRestaurantData.cs
+++ OdeToFood.Data/IRestaurantData.cs
@@ -6,7 +6,7 @@ namespace OdeToFood.Data
 {
     public interface IRestaurantData
     {
-        IEnumerable<Restaurant> GetAll();
+        IEnumerable<Restaurant> GetRestaurantsByName(string  name);
         
     }
     
@@ -24,9 +24,10 @@ namespace OdeToFood.Data
             };
         }
         
-        public IEnumerable<Restaurant> GetAll()
+        public IEnumerable<Restaurant> GetRestaurantsByName(string  name)
         {
             return from r in restaurants 
+                where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
                 orderby r.Name 
                 select r;
         }
