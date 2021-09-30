c7eee963f97f19f16222a9c62860e3f07961cf1c
mdodovic <matija.dodovic@gmail.com>
2021-09-30 11:22:28 +0200

    add Detail component

---
+++ OdeToFood/Pages/Restaurants/Detail.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class Detail : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
