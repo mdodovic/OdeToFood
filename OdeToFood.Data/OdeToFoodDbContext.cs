d519302767341ba9c7fb0c3acd9550399982da3c
mdodovic <matija.dodovic@gmail.com>
2021-10-03 10:54:17 +0200

    database connection establish

--- OdeToFood.Data/OdeToFoodDbContext.cs
+++ OdeToFood.Data/OdeToFoodDbContext.cs
@@ -1,7 +1,14 @@
-﻿namespace OdeToFood.Data
+﻿using Microsoft.EntityFrameworkCore;
+using OdeToFood.Core;
+
+namespace OdeToFood.Data
 {
-    public class OdeToFoodDbContext
+    public class OdeToFoodDbContext : DbContext
     {
+        public OdeToFoodDbContext(DbContextOptions<OdeToFoodDbContext> options): base(options)
+        {
+        }
         
+        public DbSet<Restaurant> Restaurants { get; set; }
     }
 }
